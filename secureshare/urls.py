e48dd0d743c0a6e4b1ddc42b0ad6a64a2f8cfa89
Srikanth Chelluri <schellluri@gmail.com>
2016-03-24 14:54:16 -0400

    more work on login

--- secureshare/urls.py
+++ secureshare/urls.py
@@ -4,5 +4,7 @@ from secureshare import views
 urlpatterns = [
     url(r'^$', views.login, name='login'),
     url(r'^auth/$', views.auth, name='auth'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^authregister/$', views.authregister, name='authregister'),
     url(r'^home/$', views.home, name='home'),
 ]
--- secureshare/views.py
+++ secureshare/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 
@@ -9,21 +10,34 @@ def login(request):
 
 def auth(request):
 	if request.method == 'POST':
-		username = request.POST.get('inputUsername', '')
-		password = request.POST.get('inputPassword', '')
-		user = authenticate(username=username, password=password)
+		usernameIn = request.POST.get('inputUsername', '')
+		passwordIn = request.POST.get('inputPassword', '')
+		user = authenticate(username=usernameIn, password=passwordIn)
 		if user is not None:
 			if user.is_active:
 				return home(request)
 				#print("User is valid, active, and authenticated")
 			else:
-				print("The password is valid, but thea ccount has been disable")
+				print("The password is valid, but the account has been disable")
 		else:
-			return render(request, 'secureshare/failed.html')
+			#return render(request, 'secureshare/failed.html')
+			return home(request)
 	return render(request, 'secureshare/failed.html')
 
-@login_required
+def register(request):
+	return render(request, 'secureshare/register.html')
+
+def authregister(request):
+	if request.method == 'POST':
+		usernameIn = request.POST.get('inputUsername', '')
+		emailIn = request.POST.get('inputEmail', '')
+		passwordIn = request.POST.get('inputPassword', '')
+		user = User.objects.create_user(usernameIn, emailIn, passwordIn)
+		user.save()
+		return login(request)
+
+
 def home(request):
 	if not request.user.is_authenticated():
-		return render(request, 'secureshare/login.html', {'info': "We couldn't authenticate you. Sign in again."})
+		return render(request, 'secureshare/failed.html')
 	return render(request, 'secureshare/home.html')

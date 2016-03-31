682e7e214dde355d05792552cf498f5d1505f602
Srikanth Chelluri <schellluri@gmail.com>
2016-03-31 15:31:13 -0400

    broke down tasks into pages, updated home page to follow

--- secureshare/views.py
+++ secureshare/views.py
@@ -96,7 +96,8 @@ def register(request):
         # Print problems to the terminal.
         # They'll also be shown to the user.
         else:
-            print(user_form.errors, profile_form.errors)
+            return render(request, 'secureshare/failed.html')
+            #print(user_form.errors, profile_form.errors)
 
     # Not a HTTP POST, so we render our form using two ModelForm instances.
     # These forms will be blank, ready for user input.

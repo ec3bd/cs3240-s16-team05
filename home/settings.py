029d33bcb55f7852fcd43800354e48eb567d245a
Srikanth Chelluri <schellluri.developer@gmail.com>
2016-04-05 19:46:07 -0400

    fixed all merges, tested

--- home/settings.py
+++ home/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'secureshare',
-    'registration',
 ]
 
 MIDDLEWARE_CLASSES = [
---
+++ secureshare/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('secureshare', '0004_merge'),
+        ('secureshare', '0005_message_encrypt'),
+    ]
+
+    operations = [
+    ]

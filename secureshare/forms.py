3b04c77bbe84a69d9451bf1855319aec33d32acc
Hudson Ayers <hra5th@virginia.edu>
2016-04-05 20:36:13 -0400

    fixed login stuff that got messed up

--- secureshare/forms.py
+++ secureshare/forms.py
@@ -4,10 +4,9 @@ from secureshare.models import UserProfile, UploadFile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ('username', 'email', 'password', 'password2')
+        fields = ('username', 'email', 'password')
 
 class UploadFileForm(forms.ModelForm):
   class Meta:

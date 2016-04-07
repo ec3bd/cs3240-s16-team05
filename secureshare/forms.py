31a5f54b1a4dfa8878bc66932d99ea435652ad91
mgsanusi <marigrace07@gmail.com>
2016-04-06 21:50:37 -0400

    hopefully this fixed the error

--- secureshare/forms.py
+++ secureshare/forms.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 from secureshare.models import UserProfile, UploadFile
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+  password = forms.CharField(widget=forms.PasswordInput())
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')
+  class Meta:
+    model = User
+    fields = ('username', 'email', 'password')
 
 class UploadFileForm(forms.ModelForm):
   class Meta:
@@ -15,21 +15,13 @@ class UploadFileForm(forms.ModelForm):
     fields = '__all__'
 
 class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-<<<<<<< HEAD
-        fields = ('website', 'picture')
-=======
-        fields = ('website', 'picture', 'siteManager')
->>>>>>> c5da34a21763640b54087e27c7ab50be057b0521
+  class Meta:
+    model = UserProfile
+    fields = ('website', 'picture', 'siteManager')
 
 class DocumentForm(forms.Form):
   docfile = forms.FileField(label='Select a file')
 
-class UploadFileForm(forms.ModelForm):
+class UploadFileForm(forms.Form):
   title = forms.CharField(max_length=50)
   file = forms.FileField()
-<<<<<<< HEAD
-  """
-=======
->>>>>>> c5da34a21763640b54087e27c7ab50be057b0521
